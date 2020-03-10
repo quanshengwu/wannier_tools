cfc9756eaf8950b8c61ee03b6eb46c91ea889d16
QuanSheng Wu <wuquansheng@gmail.com>
2020-03-10 20:40:24 +0100

    update

--- src/wanniercenter.f90
+++ src/wanniercenter.f90
@@ -2152,13 +2152,13 @@
          write(outfileindex, '(a)')'set xlabel "k" '
          write(outfileindex, '(a)')'set xlabel offset 0, 0.7 '
          write(outfileindex, '(a)')'set ytics 0.5 '
+         write(outfileindex, '(a)')'unset xtics '
          write(outfileindex, '(a)')'set ytics format "%4.1f" nomirror out'
          write(outfileindex, '(a)')'set ylabel "WCC"'
          write(outfileindex, '(a)')'set ylabel offset 2, 0.0 '
          write(outfileindex, '(a)')'set xrange [0: 0.5]'
          write(outfileindex, '(a)')'set yrange [0:1]'
-         write(outfileindex, '(a)')"plot 'wcc.dat' u 1:2 w l lw 2  lc 'blue', \"   
-         write(outfileindex, '(a, i5, a)')" for [i=4: ", Numoccupied+3, "] 'wcc.dat' u 1:i w p  pt 7  ps 1.1 lc 'red'"
+         write(outfileindex, '(a, i5, a)')"plot for [i=4: ", Numoccupied+3, "] 'wcc.dat' u 1:i w p  pt 7  ps 1.1 lc 'red'"
          close(outfileindex)
       endif
 
